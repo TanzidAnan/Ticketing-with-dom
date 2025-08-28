5b6db6428a58346502418254c90532290677088c
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-29 03:05:23 +0600

    Add to card class name select

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function hendleSelect(){
+    console.log(5455)
+}
+
+const allBtn =document.getElementsByClassName('add-to-btn');
+
+for(const btn of allBtn){
+    btn.addEventListener('click',function(){
+        console.log(454)
+    })
+}
